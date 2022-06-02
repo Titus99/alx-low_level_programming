6b46a9adea6abd5e26af9fd4383ab07f93f36a63
TITUS <mpe2418@yahoo.com>
2022-06-02 13:57:10 -0700

    last attempt

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,7 @@
 /**
  * main - Entry point
  *
- * Description: 'and that piece of art is useful - Dora Korpar'
- *
- * Return: Always (Success)
+ * Return: Always 0 (Success)
  *
  */
 
@@ -14,7 +12,10 @@ int main(void)
 
 {
 
-		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-		return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+
+			sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+
+	return (1);
 
 }
