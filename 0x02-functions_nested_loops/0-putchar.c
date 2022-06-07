a07439dcc5aa94af09e96acc00e68b72571e742a
TITUS <mpe2418@yahoo.com>
2022-06-07 09:30:49 -0700

    6

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,34 @@
 #include<stdio.h>
 
 /**
- * Main Entry print "_putchar"
+* main - Print the word "_putchar"
  *
- * return 0 (always) success
+ * Return: Always 0 (Success)
  *
  */
-int main (void)
+
+
+
+int main(void)
 
 {
-	printf("_putchar\n");
 
-	return (0);
+		int i;
+
+		char p[] = "Holberton";
+
+
+
+		for (i = 0; i < 9; i++)
+
+				{
+
+					_putchar(p[i]);
+
+								}
+
+					_putchar('\n');
+
+				return (0);
+
 }
