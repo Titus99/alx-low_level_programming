fb657cb79d170623335993e6beb0c5b83989a9ad
TITUS <mpe2418@yahoo.com>
2022-06-09 12:01:48 -0700

    mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,6 +3,9 @@
 
 /**
  * mul - function that multiplies two integers
+ * @a: int to be multiplied by b
+ * @b: int to be multiplied by a
+ * Return: product of a and b
  */
 
 int mul(int a, int b)
