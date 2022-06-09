0517c9e09aafb8cd0a9fbeb34c258bc3cc05c432
TITUS <mpe2418@yahoo.com>
2022-06-09 12:56:17 -0700

    main

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,15 +6,15 @@
 
 void more_numbers(void)
 {
-	int x, y;
-	for (x = 0; x < 10; x++)
-	{
-	for (y = 0; y < 15; y++)
-	{
-	if (y >= 10)
-	_putchar(y / 10 + 48);
-	_putchar(y % 10 + 48);
-	}
-	_putchar('\n');
-	}
+int x, y;
+for (x = 0; x < 10; x++)
+{
+for (y = 0; y < 15; y++)
+{
+if (y >= 10)
+_putchar(y / 10 + 48);
+_putchar(y % 10 + 48);
+}
+_putchar('\n');
+}
 }
