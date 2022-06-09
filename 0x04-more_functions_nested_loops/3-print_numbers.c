b32f58fd316e3afc12ec96bfe4b02c9c158e3eea
TITUS <mpe2418@yahoo.com>
2022-06-09 11:56:23 -0700

    main

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,13 +2,13 @@
 
 /**
  * print_numbers - prints numbers from 0 to 9
- * Description: prints numbers from 0 to 9
- * Return: void
+ * folloed by a new line
  */
 
 void print_numbers(void)
 {
 	int i;
+
 	for (i = 0; i <= 9; ++i)
 	{
 	_putchar(i);
