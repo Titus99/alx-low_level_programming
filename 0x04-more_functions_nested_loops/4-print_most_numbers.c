0cf0c07b97c91f136ff9cc8fb2d567118ed7952b
TITUS <mpe2418@yahoo.com>
2022-06-09 12:47:14 -0700

    most

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,11 +2,8 @@
 
 /**
  * print_most_numbers - print numbers from 0 to 9 except 2 and 4
- *
  * Description: print numbers from 0 to 9 except 2 and 4
- *
  * Return: return void
- *
  */
 
 void print_most_numbers(void)
@@ -17,8 +14,8 @@ void print_most_numbers(void)
 
 		for (i = 48; i <= 58; i++)
 		{
-			if (i != 2 && i != 4)
-			_putchar(i);
+		if (i != 2 && i != 4)
+		_putchar(i);
 		}
 		_putchar('\n');
 }
