a7518df1431f4e058c7d244988d1581082621160
TITUS <mpe2418@yahoo.com>
2022-06-13 09:31:28 -0700

    prints char

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,9 +11,13 @@ void puts2(char *str)
 
 {
 	int len = 0;
+
 	int l = 0;
+	
 	char *y = str;
+	
 	int z;
+	
 	while (*y != '\0')
 	{
 	y++;
@@ -21,9 +25,11 @@ void puts2(char *str)
 	}
 	l = len - 1;
 	for (z = 0 ; z <= l ; z++)
-	{										if (z % 2 == 0)
+	{
+	if (z % 2 == 0)
 	{
 	_putchar(str[z]);
-	}										}
+	}
+	}
 	_putchar('\n');
 }
