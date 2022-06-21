2ca127dfadc5bf6b7f5a5c2e7b779748f4519e27
TITUS <mpe2418@yahoo.com>
2022-06-21 06:26:37 -0700

    advanced

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -13,14 +13,14 @@ char *rot13(char *s)
 	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 	for (i = 0; *(s + i); i++)
 	{
-		for (j = 0; j < 52; j++)
-		{
-			if (a[j] == *(s + i))
-			{
-				*(s + i) = b[j];
-				break;
-			}
-		}
+	for (j = 0; j < 52; j++)
+	{
+	if (a[j] == *(s + i))
+	{
+	*(s + i) = b[j];
+	break;
+	}
+	}
 	}
 	return (s);
 }
